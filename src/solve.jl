c24885d42cf9889e86c19ae92c484d54ffcb713a
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-04-18 22:37:50 -0700

    spacing and mass matrix

--- src/solve.jl
+++ src/solve.jl
@@ -1,31 +1,35 @@
 function solve(prob::AbstractSteadyStateProblem,alg::SteadyStateDiffEqAlgorithm)
 
-    if typeof(prob.u0) <: Number
-        u0 = [prob.u0]
-    else
-        u0 = vec(deepcopy(prob.u0))
-    end
+  if prob.mass_matrix != I
+    error("This solver is not able to use mass matrices.")
+  end
 
-    sizeu = size(prob.u0)
+  if typeof(prob.u0) <: Number
+    u0 = [prob.u0]
+  else
+    u0 = vec(deepcopy(prob.u0))
+  end
 
-    if !isinplace(prob) && (typeof(prob.u0)<:AbstractVector || typeof(prob.u0)<:Number)
-      f! = (u,du) -> (du[:] = prob.f(0, u); nothing)
-    elseif !isinplace(prob) && typeof(prob.u0)<:AbstractArray
-      f! = (u,du) -> (du[:] = vec(prob.f(0, reshape(u, sizeu))); nothing)
-    elseif typeof(prob.u0)<:AbstractVector
-      f! = (u,du) -> (prob.f(0, u, du); nothing)
-    else # Then it's an in-place function on an abstract array
-      f! = (u,du) -> (prob.f(0, reshape(u, sizeu),reshape(du, sizeu));
-                      u = vec(u); du=vec(du); nothing)
-    end
+  sizeu = size(prob.u0)
 
-    # du = similar(u)
-    # f = (u) -> (f!(du,u); du) # out-of-place version
+  if !isinplace(prob) && (typeof(prob.u0)<:AbstractVector || typeof(prob.u0)<:Number)
+    f! = (u,du) -> (du[:] = prob.f(0, u); nothing)
+  elseif !isinplace(prob) && typeof(prob.u0)<:AbstractArray
+    f! = (u,du) -> (du[:] = vec(prob.f(0, reshape(u, sizeu))); nothing)
+  elseif typeof(prob.u0)<:AbstractVector
+    f! = (u,du) -> (prob.f(0, u, du); nothing)
+  else # Then it's an in-place function on an abstract array
+    f! = (u,du) -> (prob.f(0, reshape(u, sizeu),reshape(du, sizeu));
+                    u = vec(u); du=vec(du); nothing)
+  end
 
-    if typeof(alg) <: SSRootfind
-      res = alg.nlsolve(f!,u0)
-      build_solution(prob,alg,res.zero,res.residual_norm;retcode = :Success)
-    else
-      error("Algorithm not recognized")
-    end
+  # du = similar(u)
+  # f = (u) -> (f!(du,u); du) # out-of-place version
+
+  if typeof(alg) <: SSRootfind
+    res = alg.nlsolve(f!,u0)
+    build_solution(prob,alg,res.zero,res.residual_norm;retcode = :Success)
+  else
+    error("Algorithm not recognized")
+  end
 end
