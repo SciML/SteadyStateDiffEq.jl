55d222819acb363e8880b71c5a9aa25ad3eb28ee
Takafumi Arakaki <aka.tkf@gmail.com>
2018-10-17 22:21:53 -0700

    Add tspan option to DynamicSS

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,7 @@ u0 = zeros(2)
 prob = SteadyStateProblem(f,u0)
 abstol = 1e-8
 sol = solve(prob,SSRootfind())
+@test sol.retcode == :Success
 p = nothing
 
 du = zeros(2)
@@ -18,22 +19,29 @@ f(du,sol.u,nothing,0)
 prob = ODEProblem(f,u0,(0.0,1.0))
 prob = SteadyStateProblem(prob)
 sol = solve(prob,SSRootfind(nlsolve = (f,u0,abstol) -> (res=NLsolve.nlsolve(f,u0,autodiff=:forward,method=:newton,iterations=Int(1e6),ftol=abstol);res.zero) ))
+@test sol.retcode == :Success
 
 f(du,sol.u,nothing,0)
 @test du == [0,0]
 
 # Use Sundials
 sol = solve(prob,SSRootfind(nlsolve = (f,u0,abstol) -> (res=Sundials.kinsol(f,u0)) ))
+@test sol.retcode == :Success
 f(du,sol.u,nothing,0)
 @test du == [0,0]
 
 using OrdinaryDiffEq
 sol = solve(prob,DynamicSS(Rodas5()))
+@test sol.retcode == :Success
 
 f(du,sol.u[end],p,0)
 @test du ≈ [0,0] atol = 1e-7
 
+sol = solve(prob,DynamicSS(Rodas5(),tspan=1e-3))
+@test sol.retcode != :Success
+
 sol = solve(prob,DynamicSS(CVODE_BDF()),dt=1.0)
+@test sol.retcode == :Success
 
 f(du,sol.u[end],p,0)
 @test du ≈ [0,0] atol = 1e-6
