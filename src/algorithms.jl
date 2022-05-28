2e89347dc456656437159543de9d334ffec5cb8b
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-05-27 23:22:18 -0400

    add a bunch of algorithm traits from SciMLBase

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -17,5 +17,14 @@ DynamicSS(alg; abstol = 1e-8, reltol = 1e-6, tspan = Inf) =
 # Backward compatibility:
 DynamicSS(alg, abstol, reltol) = DynamicSS(alg; abstol=abstol, reltol=reltol)
 
+## SciMLBase Trait Definitions
+
+SciMLBase.isadaptive(alg::SteadyStateDiffEqAlgorithm) = true
+
 SciMLBase.isautodifferentiable(alg::SSRootfind) = true
+SciMLBase.allows_arbitrary_number_types(alg::SSRootfind) = true
+SciMLBase.allowscomplex(alg::SSRootfind) = true
+
 SciMLBase.isautodifferentiable(alg::DynamicSS) = SciMLBase.isautodifferentiable(alg.alg)
+SciMLBase.allows_arbitrary_number_types(alg::DynamicSS) = SciMLBase.allows_arbitrary_number_types(alg.alg)
+SciMLBase.allowscomplex(alg::DynamicSS) = SciMLBase.allowscomplex(alg.alg)
