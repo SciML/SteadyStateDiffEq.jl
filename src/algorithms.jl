9bf85ed333c7be685e12f5baeae6204f4d60b3f8
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-23 14:55:25 -0400

    format SciML Style

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1,21 +1,27 @@
 abstract type SteadyStateDiffEqAlgorithm <: DiffEqBase.AbstractSteadyStateAlgorithm end
 
 struct SSRootfind{F} <: SteadyStateDiffEqAlgorithm
-  nlsolve::F
+    nlsolve::F
 end
-SSRootfind(;nlsolve=(f,u0,abstol) -> (res=NLsolve.nlsolve(f,u0,ftol = abstol);res.zero)) = SSRootfind(nlsolve)
+function SSRootfind(;
+                    nlsolve = (f, u0, abstol) -> (res = NLsolve.nlsolve(f, u0,
+                                                                        ftol = abstol);
+                                                  res.zero))
+    SSRootfind(nlsolve)
+end
 
-struct DynamicSS{A,AT,RT,TS} <: SteadyStateDiffEqAlgorithm
-  alg::A
-  abstol::AT
-  reltol::RT
-  tspan::TS
+struct DynamicSS{A, AT, RT, TS} <: SteadyStateDiffEqAlgorithm
+    alg::A
+    abstol::AT
+    reltol::RT
+    tspan::TS
 end
-DynamicSS(alg; abstol = 1e-8, reltol = 1e-6, tspan = Inf) =
-  DynamicSS(alg, abstol, reltol, tspan)
+function DynamicSS(alg; abstol = 1e-8, reltol = 1e-6, tspan = Inf)
+    DynamicSS(alg, abstol, reltol, tspan)
+end
 
 # Backward compatibility:
-DynamicSS(alg, abstol, reltol) = DynamicSS(alg; abstol=abstol, reltol=reltol)
+DynamicSS(alg, abstol, reltol) = DynamicSS(alg; abstol = abstol, reltol = reltol)
 
 ## SciMLBase Trait Definitions
 
@@ -26,5 +32,7 @@ SciMLBase.allows_arbitrary_number_types(alg::SSRootfind) = true
 SciMLBase.allowscomplex(alg::SSRootfind) = true
 
 SciMLBase.isautodifferentiable(alg::DynamicSS) = SciMLBase.isautodifferentiable(alg.alg)
-SciMLBase.allows_arbitrary_number_types(alg::DynamicSS) = SciMLBase.allows_arbitrary_number_types(alg.alg)
+function SciMLBase.allows_arbitrary_number_types(alg::DynamicSS)
+    SciMLBase.allows_arbitrary_number_types(alg.alg)
+end
 SciMLBase.allowscomplex(alg::DynamicSS) = SciMLBase.allowscomplex(alg.alg)
