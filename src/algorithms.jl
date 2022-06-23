bb361c3259bdb6c1d8b7caaa12595283f1a3a058
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-23 14:48:54 -0400

    format SciML Style

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1,21 +1,20 @@
 abstract type SteadyStateDiffEqAlgorithm <: DiffEqBase.AbstractSteadyStateAlgorithm end
 
 struct SSRootfind{F} <: SteadyStateDiffEqAlgorithm
-  nlsolve::F
+    nlsolve::F
 end
-SSRootfind(;nlsolve=(f,u0,abstol) -> (res=NLsolve.nlsolve(f,u0,ftol = abstol);res.zero)) = SSRootfind(nlsolve)
+SSRootfind(; nlsolve = (f, u0, abstol) -> (res = NLsolve.nlsolve(f, u0; ftol = abstol); res.zero)) = SSRootfind(nlsolve)
 
 struct DynamicSS{A,AT,RT,TS} <: SteadyStateDiffEqAlgorithm
-  alg::A
-  abstol::AT
-  reltol::RT
-  tspan::TS
+    alg::A
+    abstol::AT
+    reltol::RT
+    tspan::TS
 end
-DynamicSS(alg; abstol = 1e-8, reltol = 1e-6, tspan = Inf) =
-  DynamicSS(alg, abstol, reltol, tspan)
+DynamicSS(alg; abstol = 1e-8, reltol = 1e-6, tspan = Inf) = DynamicSS(alg, abstol, reltol, tspan)
 
 # Backward compatibility:
-DynamicSS(alg, abstol, reltol) = DynamicSS(alg; abstol=abstol, reltol=reltol)
+DynamicSS(alg, abstol, reltol) = DynamicSS(alg; abstol = abstol, reltol = reltol)
 
 ## SciMLBase Trait Definitions
 
