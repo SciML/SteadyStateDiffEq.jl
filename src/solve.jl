4a0d6ff44d9e2002edc830fc9bd7605b74566d42
Avik Pal <avikpal@mit.edu>
2023-02-15 13:30:08 -0500

    Propagate the destats

--- src/solve.jl
+++ src/solve.jl
@@ -133,5 +133,5 @@ function DiffEqBase.__solve(prob::DiffEqBase.AbstractSteadyStateProblem,
         u = sol.u[idx]
     end
 
-    return DiffEqBase.build_solution(prob, alg, u, du; retcode)
+    return DiffEqBase.build_solution(prob, alg, u, du; retcode, stats = sol.destats)
 end
