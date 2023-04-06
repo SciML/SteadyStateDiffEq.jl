8987c93caf080f52554468805ad05761e78532d0
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2023-04-06 04:46:00 +0200

    Added callback functionality

--- src/solve.jl
+++ src/solve.jl
@@ -93,10 +93,6 @@ function DiffEqBase.__solve(prob::DiffEqBase.AbstractSteadyStateProblem,
         end
     end
 
-    #= if isdefined(Main, :Infiltrator)
-        Main.infiltrate(@__MODULE__, Base.@locals, @__FILE__, @__LINE__)
-    end =#
-
     mode = DiffEqBase.get_termination_mode(alg.termination_condition)
 
     if mode ∈ DiffEqBase.SAFE_BEST_TERMINATION_MODES && !save_everystep
@@ -104,11 +100,14 @@ function DiffEqBase.__solve(prob::DiffEqBase.AbstractSteadyStateProblem,
     end
 
     storage = mode ∈ DiffEqBase.SAFE_TERMINATION_MODES ? Dict() : nothing
-    cb1 = TerminateSteadyState(alg.termination_condition.abstol,
+    callback = TerminateSteadyState(alg.termination_condition.abstol,
                                alg.termination_condition.reltol,
                                alg.termination_condition(storage))
 
-    callback = CallbackSet(cb1, kwargs[:callback])
+    if haskey(kwargs, :callback)
+        callback = CallbackSet(callback, kwargs[:callback])
+    end
+
     _prob = ODEProblem(f, prob.u0, tspan, prob.p)
     sol = solve(_prob, alg.alg, args...; kwargs..., save_everystep, save_start, callback)
 
