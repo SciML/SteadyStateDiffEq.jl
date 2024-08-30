f9b37c72de34d03196a60da6a18905aff69e1cfa
Avik Pal <avikpal@mit.edu>
2024-08-30 10:50:24 -0400

    chore: minor updates to SteadyStateDiff

--- src/SteadyStateDiffEq.jl
+++ src/SteadyStateDiffEq.jl
@@ -1,9 +1,19 @@
 module SteadyStateDiffEq
 
-using Reexport
+using Reexport: @reexport
 @reexport using DiffEqBase
 
-using DiffEqCallbacks, ConcreteStructs, LinearAlgebra, SciMLBase
+using ConcreteStructs: @concrete
+using DiffEqBase: DiffEqBase, AbstractNonlinearTerminationMode,
+                  AbstractSafeNonlinearTerminationMode,
+                  AbstractSafeBestNonlinearTerminationMode,
+                  NonlinearSafeTerminationReturnCode, NormTerminationMode
+using DiffEqCallbacks: TerminateSteadyState
+using LinearAlgebra: norm
+using SciMLBase: SciMLBase, CallbackSet, NonlinearProblem, ODEProblem,
+                 ReturnCode, SteadyStateProblem, get_du, init, isinplace
+
+const infnorm = Base.Fix2(norm, Inf)
 
 include("algorithms.jl")
 include("solve.jl")
--- src/solve.jl
+++ src/solve.jl
@@ -1,7 +1,7 @@
-function DiffEqBase.__solve(prob::DiffEqBase.AbstractSteadyStateProblem, alg::SSRootfind,
+function SciMLBase.__solve(prob::SciMLBase.AbstractSteadyStateProblem, alg::SSRootfind,
         args...; kwargs...)
     nlprob = NonlinearProblem(prob)
-    nlsol = DiffEqBase.__solve(nlprob, alg.alg, args...; kwargs...)
+    nlsol = SciMLBase.__solve(nlprob, alg.alg, args...; kwargs...)
     return SciMLBase.build_solution(prob, SSRootfind(nlsol.alg), nlsol.u, nlsol.resid;
         nlsol.retcode, nlsol.stats, nlsol.left, nlsol.right, original = nlsol)
 end
@@ -9,12 +9,11 @@ end
 __get_tspan(u0, alg::DynamicSS) = __get_tspan(u0, alg.tspan)
 __get_tspan(u0, tspan::Tuple) = tspan
 function __get_tspan(u0, tspan::Number)
-    return convert.(DiffEqBase.value(real(eltype(u0))),
-        (DiffEqBase.value(zero(tspan)), tspan))
+    return convert.(
+        DiffEqBase.value(real(eltype(u0))), (DiffEqBase.value(zero(tspan)), tspan))
 end
 
-infnorm(x) = norm(x,Inf)
-function DiffEqBase.__solve(prob::DiffEqBase.AbstractSteadyStateProblem, alg::DynamicSS,
+function SciMLBase.__solve(prob::SciMLBase.AbstractSteadyStateProblem, alg::DynamicSS,
         args...; abstol = 1e-8, reltol = 1e-6, odesolve_kwargs = (;),
         save_idxs = nothing, termination_condition = NormTerminationMode(infnorm),
         kwargs...)
@@ -54,7 +53,7 @@ function DiffEqBase.__solve(prob::DiffEqBase.AbstractSteadyStateProblem, alg::Dy
 
     # Construct and solve the ODEProblem
     odeprob = ODEProblem{isinplace(prob)}(f, prob.u0, tspan, prob.p)
-    odesol = DiffEqBase.__solve(odeprob, alg.alg, args...; abstol, reltol, kwargs...,
+    odesol = SciMLBase.__solve(odeprob, alg.alg, args...; abstol, reltol, kwargs...,
         odesolve_kwargs..., callback, save_end = true)
 
     resid, u, retcode = __get_result_from_sol(termination_condition, tc_cache, odesol)
@@ -68,8 +67,7 @@ function DiffEqBase.__solve(prob::DiffEqBase.AbstractSteadyStateProblem, alg::Dy
         retcode, original = odesol)
 end
 
-function __get_result_from_sol(::DiffEqBase.AbstractNonlinearTerminationMode, tc_cache,
-        odesol)
+function __get_result_from_sol(::AbstractNonlinearTerminationMode, tc_cache, odesol)
     u, t = last(odesol.u), last(odesol.t)
     du = odesol(t, Val{1})
     return (du, u,
@@ -77,18 +75,15 @@ function __get_result_from_sol(::DiffEqBase.AbstractNonlinearTerminationMode, tc
             ReturnCode.Failure))
 end
 
-function __get_result_from_sol(::DiffEqBase.AbstractSafeNonlinearTerminationMode, tc_cache,
-        odesol)
+function __get_result_from_sol(::AbstractSafeNonlinearTerminationMode, tc_cache, odesol)
     u, t = last(odesol.u), last(odesol.t)
     du = odesol(t, Val{1})
 
-    if tc_cache.retcode == DiffEqBase.NonlinearSafeTerminationReturnCode.Success
+    if tc_cache.retcode == NonlinearSafeTerminationReturnCode.Success
         retcode_tc = ReturnCode.Success
-    elseif tc_cache.retcode ==
-           DiffEqBase.NonlinearSafeTerminationReturnCode.PatienceTermination
+    elseif tc_cache.retcode == NonlinearSafeTerminationReturnCode.PatienceTermination
         retcode_tc = ReturnCode.ConvergenceFailure
-    elseif tc_cache.retcode ==
-           DiffEqBase.NonlinearSafeTerminationReturnCode.ProtectiveTermination
+    elseif tc_cache.retcode == NonlinearSafeTerminationReturnCode.ProtectiveTermination
         retcode_tc = ReturnCode.Unstable
     else
         retcode_tc = ReturnCode.Default
@@ -105,18 +100,15 @@ function __get_result_from_sol(::DiffEqBase.AbstractSafeNonlinearTerminationMode
     return du, u, retcode
 end
 
-function __get_result_from_sol(::DiffEqBase.AbstractSafeBestNonlinearTerminationMode,
-        tc_cache, odesol)
+function __get_result_from_sol(::AbstractSafeBestNonlinearTerminationMode, tc_cache, odesol)
     u, t = tc_cache.u, only(DiffEqBase.get_saved_values(tc_cache))
     du = odesol(t, Val{1})
 
-    if tc_cache.retcode == DiffEqBase.NonlinearSafeTerminationReturnCode.Success
+    if tc_cache.retcode == NonlinearSafeTerminationReturnCode.Success
         retcode_tc = ReturnCode.Success
-    elseif tc_cache.retcode ==
-           DiffEqBase.NonlinearSafeTerminationReturnCode.PatienceTermination
+    elseif tc_cache.retcode == NonlinearSafeTerminationReturnCode.PatienceTermination
         retcode_tc = ReturnCode.ConvergenceFailure
-    elseif tc_cache.retcode ==
-           DiffEqBase.NonlinearSafeTerminationReturnCode.ProtectiveTermination
+    elseif tc_cache.retcode == NonlinearSafeTerminationReturnCode.ProtectiveTermination
         retcode_tc = ReturnCode.Unstable
     else
         retcode_tc = ReturnCode.Default
