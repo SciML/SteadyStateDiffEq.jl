eeafb3868b1c672c831580e36bc074933c0ea7a5
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-11 10:17:20 -0400

    Use SciMLTesting public API docs QA
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -1,12 +1,22 @@
 using SciMLTesting, SteadyStateDiffEq, Test
 using JET
 
-include("public_api_docs.jl")
+dependency_reexports(pkg) = Tuple(
+    name for name in public_api_names(pkg)
+        if isdefined(pkg, name) && parentmodule(getfield(pkg, name)) !== pkg
+)
+
+const DEPENDENCY_REEXPORTS = dependency_reexports(SteadyStateDiffEq)
 
 run_qa(
     SteadyStateDiffEq;
     explicit_imports = true,
     jet_kwargs = (; target_defined_modules = true),
+    api_docs_kwargs = (;
+        rendered = true,
+        ignore = DEPENDENCY_REEXPORTS,
+        rendered_ignore = DEPENDENCY_REEXPORTS,
+    ),
     # Aqua deps_compat: missing [compat] for stdlib LinearAlgebra
     # https://github.com/SciML/SteadyStateDiffEq.jl/issues/134
     aqua_broken = (:deps_compat,),
